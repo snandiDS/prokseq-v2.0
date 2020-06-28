6effc7b38bd377ea76082b988a162eef7f5683f8
snandids <soumyadeep.nandi@gmail.com>
2020-06-28 17:03:48 +0530

    first commit

--- scripts/samtools.sh
+++ scripts/samtools.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+# Author : Soumyadeep Nandi
+# The script will compile the samtools version 1.10 in the depend
+# folder. The script will create a folder "samtools" to store the
+# compiled programs.
+
 current=$(pwd)
 echo $current
 dest="${current}/depend/samtools"
